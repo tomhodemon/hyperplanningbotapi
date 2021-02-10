855b3f668403654b3e98a647aea0b83960f076fc
Hodemon <tom_hodemon@outlook.fr>
2021-02-10 07:37:48 +0100

    fix clock issue

--- clock.py
+++ clock.py
@@ -3,11 +3,29 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from models import User, Base
 from database import Session, engine
 import utils
-from config import TZ
+#from config import TZ
 
-sched = BlockingScheduler(tz=TZ)
+# sched = BlockingScheduler(tz=TZ)
+sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=7) # run every weekday at 7:00am
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=7) # run every weekday at 7:00am
+# def processingDataJob():
+
+#     s = Session()
+
+#     Base.metadata.bind = engine
+#     Course = Base.metadata.tables['courses']
+#     Course.drop(engine)
+#     Course.create(engine)
+
+#     users = s.query(User).all()
+#     utils.processingData(users, s)
+    
+#     print("Data has been successfully collected")
+
+#     s.close()
+
+@sched.scheduled_job('interval', hours=1)
 def processingDataJob():
 
     s = Session()
